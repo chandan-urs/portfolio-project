6c46901833761f66b0992216b5074dca8dd069fd
unknown <p.chandan19952@gmail.com>
2019-07-27 21:38:19 +0530

    blog model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/")
+
