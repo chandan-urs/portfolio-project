925f07a7ed6699bbf410c0ad9d0adc628f836977
unknown <p.chandan19952@gmail.com>
2019-07-28 14:19:59 +0530

    blog details created

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=255)
@@ -7,3 +8,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pubdate.strftime('%b %e %Y')
