3db46fa7b0846a219dd4c60c30e0acd8ee1f4633
chandan-urs <p.chandan1995@gmail.com>
2019-11-15 23:25:59 -0800

    docker configuration complete

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'z^f60kge57k&$#=n=fkwh$6xz^ms-j4g0$%jd6tyfgut$5&q3n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost']
 
 # Application definition
 
